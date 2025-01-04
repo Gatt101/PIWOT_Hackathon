7f079a8477ff89eab403131ce55e0cb90ad611e2
Chirag Patil <chinu7374@gmail.com>
2025-01-04 11:12:06 +0530

    Sign up and Login added in Frontend

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,6 +1,6 @@
+import toast from "react-hot-toast";
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
-
 
 export const useAuthStore = create ((set)=> ({
   //States
@@ -26,5 +26,40 @@ export const useAuthStore = create ((set)=> ({
  
     signup: async(data) => {
 
+      set({ isSigningUp: true});
+       try {
+          const res = await axiosInstance.post("/auth/signup",data);
+          set({ authUser: res.data});
+          toast.success("Account created successfully");
+
+       } catch (error) {
+          toast.error(error.response.data.message);
+       }finally{
+            set({ isSigningUp:false});
+       }
+    },
+    login: async (data) => {
+      set({ isLoggingIn: true });
+      try {
+        const res = await axiosInstance.post("/auth/login", data);
+        set({ authUser: res.data });
+        toast.success("Logged in successfully");
+  
+        // get().connectSocket();
+      } catch (error) {
+        toast.error(error.response.data.message);
+      } finally {
+        set({ isLoggingIn: false });
+      }
+    },
+
+    logout : async() => {
+      try {
+        await axiosInstance.post("/auth/logout");
+        set({authUser: null});
+        toast.success("Logged out successfully");
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
     }
 }))
