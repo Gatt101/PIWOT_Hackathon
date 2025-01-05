bf468aa82241a8802750f23a6812f983f3577574
Chirag Patil <chinu7374@gmail.com>
2025-01-05 10:28:17 +0530

    Home Page Initiated

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,39 @@
+ import toast from "react-hot-toast";
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+
+export const useChatStore = create((set) => ({
+  message:[],
+  users:[],
+  selectedUser:null,
+  isUsersLoading: false,
+  isMessageLoading: false ,
+
+  getUsers: async () => {
+      set({ isUsersLoading: true});
+      try{
+        const res = await axiosInstance.get("/messages/users");
+        set({ users: res.data })
+      }catch(error){
+        toast.error(error.response.data.message);
+      }finally{
+        set({ isUsersLoading: false})
+      }
+  },
+
+  getMessages: async(userId) => {
+    set({ isMessageLoading:true });
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: res.data})
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }finally{
+      set({ isMessageLoading: false });
+    }
+  },
+
+  setSelectedUser: async(selectedUser) => ({ selectedUser}),
+
+}))
+  
